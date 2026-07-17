172c31a609ea4607b207c4711afc53ebf08766b2
Fabrizio Vinciguerra <ska@MacBook-Pro-di-Fabrizio.local>
2026-07-17 10:15:37 +0200

    aggiornamento home

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,11 @@
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("js");
+
+  // Durante lo sviluppo serve direttamente i file sorgente.
+  // Evita copie CSS vecchie dentro _site.
+  eleventyConfig.setServerPassthroughCopyBehavior("passthrough");
 
   return {
     dir: {
